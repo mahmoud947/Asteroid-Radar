55ea357ae6c1f42c29f0cd00b0590fc890955aeb
Mahmoud Kamal Fawzy <65209396+mahmoud947@users.noreply.github.com>
2022-10-25 00:52:16 +0200

    add connectTimeout and readTimeout to avoid slow API response time

--- app/src/main/java/com/udacity/asteroidradar/data/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/data/Constants.kt
@@ -5,4 +5,6 @@ object Constants {
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
     const val API_KEY ="alnvUuTKwyWf49LnxaCpfACalzvLUAZwqYrdYX3X"
+    const val CONNECTION_TIMEOUT =30L
+    const val READ_TIMEOUT =30L
 }
--- app/src/main/java/com/udacity/asteroidradar/data/remote/AsteroidsNeoWsService.kt
+++ app/src/main/java/com/udacity/asteroidradar/data/remote/AsteroidsNeoWsService.kt
@@ -1,13 +1,25 @@
 package com.udacity.asteroidradar.data.remote
 
 import com.udacity.asteroidradar.data.Constants
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
+import java.util.concurrent.TimeUnit
+
+/**
+ * set connection and read time out
+ * to avoid slow API response time
+ */
+private val clint:OkHttpClient = OkHttpClient.Builder()
+    .connectTimeout(Constants.CONNECTION_TIMEOUT,TimeUnit.SECONDS)
+    .readTimeout(Constants.READ_TIMEOUT,TimeUnit.SECONDS)
+    .build()
 
 private val retrofit: Retrofit = Retrofit.Builder()
     .baseUrl(Constants.BASE_URL)
     .addConverterFactory(ScalarsConverterFactory.create())
+    .client(clint)
     .build()
 
 
