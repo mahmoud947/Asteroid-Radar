3a88c83818b17a8627ad37d2a34a1e3b5888a4fe
Mahmoud Kamal Fawzy <65209396+mahmoud947@users.noreply.github.com>
2022-10-25 02:33:00 +0200

    create mapping functions that convert from AsteroidDto to AsteroidEntity

--- app/src/main/java/com/udacity/asteroidradar/data/remote/dto/AsteroidDto.kt
+++ app/src/main/java/com/udacity/asteroidradar/data/remote/dto/AsteroidDto.kt
@@ -9,40 +9,78 @@ import com.udacity.asteroidradar.data.local.entities.AsteroidEntity
 
 
 data class AsteroidDto(
-    val id:String,
+    val id: String,
+    val name: String,
     @Json(name = "absolute_magnitude_h")
-    val absoluteMagnitude:Double,
+    val absoluteMagnitude: Double,
     @Json(name = "estimated_diameter")
     val estimatedDiameterDto: EstimatedDiameterDto,
     @Json(name = "is_potentially_hazardous_asteroid")
-    val isPotentiallyHazardous:Boolean,
+    val isPotentiallyHazardous: Boolean,
     @Json(name = "close_approach_data")
     val closeApproachDataDto: List<CloseApproachDataDto>
 
 )
 
 data class EstimatedDiameterDto(
-val kilometers:KilometersDto
+    val kilometers: KilometersDto
 )
 
 data class KilometersDto(
     @Json(name = "estimated_diameter_max")
-    val estimatedDiameterMax:Double
+    val estimatedDiameterMax: Double
 )
 
 data class CloseApproachDataDto(
+    @Json(name = "close_approach_date")
+    val closeApproachDate: String,
     @Json(name = "relative_velocity")
-    val relativeVelocity:RelativeVelocityDto,
+    val relativeVelocity: RelativeVelocityDto,
     @Json(name = "miss_distance")
-    val miss_distance:MissDistanceDto
+    val missDistance: MissDistanceDto
 )
 
 data class RelativeVelocityDto(
     @Json(name = "kilometers_per_second")
-    val kilometersPerSecond:String
+    val kilometersPerSecond: String
 )
 
 data class MissDistanceDto(
     @Json(name = "astronomical")
-    val astronomical:String
+    val astronomical: String
 )
+
+/**
+ * mapping functions
+ * convert AsteroidDto to AsteroidEntity that will use with room database
+ */
+fun AsteroidDto.toAsteroidEntity(): AsteroidEntity =
+    AsteroidEntity(
+        id = this.id.toLong(),
+        codename = this.name,
+        absoluteMagnitude = this.absoluteMagnitude,
+        closeApproachDate = this.closeApproachDataDto[0].closeApproachDate,
+        distanceFromEarth = this.closeApproachDataDto[0].missDistance.astronomical.toDouble(),
+        relativeVelocity = this.closeApproachDataDto[0].relativeVelocity.kilometersPerSecond.toDouble(),
+        estimatedDiameter = this.estimatedDiameterDto.kilometers.estimatedDiameterMax,
+        isPotentiallyHazardous = this.isPotentiallyHazardous
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
